222e8e9715f4add8fd86b30ff64e52e923a9a481
emhoka <eunymhoka@gmail.com>
2023-05-30 20:16:06 +0200

    banner

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,7 +3,20 @@ import React from "react";
 const Filter =()=>{
     return(
         <>
-        Filter
+       <div className="flex items-center justify-between">
+       <div class="bg-white rounded shadow border p-6 w-64">
+  <h5 class="text-3xl font-bold mb-4 mt-0">My Title</h5>
+  <p class="text-gray-700 text-sm">Content goes here</p>
+</div>
+<div class="bg-white rounded shadow border p-6 w-64">
+  <h5 class="text-3xl font-bold mb-4 mt-0">My Title</h5>
+  <p class="text-gray-700 text-sm">Content goes here</p>
+</div>
+<div class="bg-white rounded shadow border p-6 w-64">
+  <h5 class="text-3xl font-bold mb-4 mt-0">My Title</h5>
+  <p class="text-gray-700 text-sm">Content goes here</p>
+</div>
+        </div>>
         </>
     )
 }
